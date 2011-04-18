3def9ed002310cde53c1adb20768cdf1fcabdda4
Niederb <thomas@niederberger.ch>
2011-04-18 05:10:01 +0000

    git-svn-id: https://minnie.zhaw.ch/svn/distanztool/DistanceToolCore@50 faa62320-0647-4c55-9a10-af1b984ca1a2

---
+++ Matlab/mds.m
@@ -0,0 +1,24 @@
+
+
+%cities=[0 93 82 133; 93 0 52 60; 82 52 0 111; 133 60 111 0]
+
+n=length(cities);
+squared = cities.*cities;
+J=eye(n)-1/n*ones(n);
+B=-1/2*J*squared*J;
+B
+
+[eigenvektorMatrix,eigenwertMatrix] = eig(B); 
+eigenwerte = diag(eigenwertMatrix);
+[ew1 index] = max(eigenwerte);
+ev1 = eigenvektorMatrix(:,index);
+eigenwerte(index) = 0;
+[ew2 index] = max(eigenwerte);
+ev2 = eigenvektorMatrix(:,index);
+
+[ev1 ev2]
+diag([sqrt(ew1) sqrt(ew2)])
+
+coord = [ev1 ev2]*diag([sqrt(ew1) sqrt(ew2)])
+
+scatter(coord(:,1), coord(:,2))
