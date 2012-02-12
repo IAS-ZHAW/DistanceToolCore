ad54ca0542a610b12e9c03aa22b1af2d8d3382f7
Thomas Niederberger <thomas@niederberger.ch>
2012-02-12 15:30:34 +0100

    added licence headers

--- src/FileTransformer.java
+++ src/FileTransformer.java
@@ -1,3 +1,8 @@
+/* released under bsd licence
+ * see LICENCE file or http://www.opensource.org/licenses/bsd-license.php for details
+ * Institute of Applied Simulation (ZHAW)
+ * Author Thomas Niederberger
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
